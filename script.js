5cf57b5d3361f2dbd1f9ede8dc73492bd8de2be8
TyTy <Tylieafter1@gmail.com>
2020-07-30 17:59:24 +0000

    cleaned and organized code

--- script.js
+++ script.js
@@ -21,24 +21,30 @@ function setup() {
 function draw() {
  background(color(0,0,100));
 
- myBall_top = myBall_yPos - ballSize/2,
- myBall_bottom = myBall_yPos + ballSize/2, 
- myBall_left = myBall_xPos - ballSize/2,
-  myBall_right = myBall_xPos + ballSize/2;
+ moveAndBounce();
+ 
+// this makes the ball appear
+rect(myBall_xPos, myBall_yPos, ballSize, ballSize);
+}
 
+function moveAndBounce(){
+	myBall_xPos  = myBall_xPos + myBall_xVel;
+	myBall_left  = myBall_xPos - ballSize/2;
+	myBall_right = myBall_xPos + ballSize/2;
  // rect(myBall_xPos, myBall_yPos, ballSize, ballSize);
   if (myBall_xPos > canvasWidth){
     myBall_xVel = -1;
   }else if (myBall_xPos < 0){
   myBall_xVel = 1;
 }
+
+	myBall_yPos  = myBall_yPos + myBall_yVel;
+	myBall_left  = myBall_yPos - ballSize/2;
+	myBall_right = myBall_yPos + ballSize/2;
 // this makes the ball go back and forth vertical
- myBall_yPos = myBall_yPos + myBall_yVel;
   if (myBall_yPos > canvasHeight){
     myBall_yVel = -1;
   }else if (myBall_yPos < 0){
   myBall_yVel = 1;
 }
-// this makes the ball appear
-rect(myBall_xPos, myBall_yPos, ballSize, ballSize);
 }
