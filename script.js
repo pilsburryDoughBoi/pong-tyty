274eb181d293eeffbf2e106db23d6417f8d18e24
TyTy <Tylieafter1@gmail.com>
2020-07-30 17:50:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+var canvasWidth = 500, canvasHeight = 400;
+var myBall;
+var ballSize = 20;
+console.log(ballSize)
+var myBall_xPos = canvasWidth/2, myBall_yPos = canvasHeight/2;
+var myBall_xVel = 1, myBall_yVel = 1;
+var myBall_top = myBall_yPos - ballSize/2,
+ myBall_bottom = myBall_yPos + ballSize/2, 
+ myBall_left = myBall_xPos - ballSize/2,
+  myBall_right = myBall_xPos + ballSize/2;
+
+
+function setup() {
+	createCanvas(canvasWidth, canvasHeight);
+  background(color(0,0,100));
+  rectMode(CENTER);
+  myBall = rect(myBall_xPos, myBall_yPos, ballSize, ballSize);
+//console.log(myBall);
+}
+
+function draw() {
+ background(color(0,0,100));
+
+ myBall_top = myBall_yPos - ballSize/2,
+ myBall_bottom = myBall_yPos + ballSize/2, 
+ myBall_left = myBall_xPos - ballSize/2,
+  myBall_right = myBall_xPos + ballSize/2;
+
+ // rect(myBall_xPos, myBall_yPos, ballSize, ballSize);
+  if (myBall_xPos > canvasWidth){
+    myBall_xVel = -1;
+  }else if (myBall_xPos < 0){
+  myBall_xVel = 1;
+}
+// this makes the ball go back and forth vertical
+ myBall_yPos = myBall_yPos + myBall_yVel;
+  if (myBall_yPos > canvasHeight){
+    myBall_yVel = -1;
+  }else if (myBall_yPos < 0){
+  myBall_yVel = 1;
+}
+// this makes the ball appear
+rect(myBall_xPos, myBall_yPos, ballSize, ballSize);
+}
