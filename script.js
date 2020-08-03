c1feb95282fb13e05a9d29e00148763a9092aeea
TyTy <Tylieafter1@gmail.com>
2020-08-03 15:38:32 +0000

    collision paddles

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ var canvasWidth = 500, canvasHeight = 400;
 var myBall;
 var ballSize = 10;
 console.log(ballSize)
-var myBall_xPos = canvasWidth/2, myBall_yPos = canvasHeight/2;
-var myBall_xVel = 1, myBall_yVel = 1;
-var myBall_top = myBall_yPos - ballSize/2,
+var myBall_xPos = canvasWidth/2, 
+    myBall_yPos = canvasHeight/2, myBall_xVel = 1, myBall_yVel = 1,  
+    myBall_top = myBall_yPos - ballSize/2,
     myBall_bottom = myBall_yPos + ballSize/2, 
     myBall_left = myBall_xPos - ballSize/2,
-    myBall_right = myBall_xPos + ballSize/2,
-    myBall.setCollider("rectangle", myBall_xPos, myBall_yPos, myBall_top, myBall_bottom);
-var paddleWidth = 9, paddleHeight = 46, paddleVel = 5, paddleL_yPos = canvasHeight/2, paddleR_yPos = canvasHeight/2, paddleL_xPos = ballSize, paddleR_xPos = canvasWidth-ballSize, paddle_top = paddleL_yPos - paddleHeight/2, paddle_bottom = paddleL_yPos + paddleHeight/2, paddle_top = paddleR_yPos - paddleHeight/2, paddle_bottom = paddleR_yPos + paddleHeight/2;    
+    myBall_right = myBall_xPos + ballSize/2;
+var paddleWidth = 9, 
+    paddleHeight = 46, 
+    paddleVel = 5, 
+    paddleL_yPos = canvasHeight/2, 
+    paddleR_yPos = canvasHeight/2, 
+    paddleL_xPos = ballSize,
+    paddleR_xPos = canvasWidth-ballSize,
+    paddle_top = paddleL_yPos - paddleHeight/2, paddle_bottom = paddleL_yPos + paddleHeight/2, paddle_top = paddleR_yPos - paddleHeight/2, paddle_bottom = paddleR_yPos + paddleHeight/2;    
 var score = 0;
 var r = 0, g = 0, b = 0;
 
@@ -19,6 +25,7 @@ function setup() {
   background(color(0,0,100));
   rectMode(CENTER);
   myBall = rect(myBall_xPos, myBall_yPos, ballSize, ballSize);
+  
  //console.log(myBall);
  myBall_xVel = random(-4,4);
  myBall_yPos = random(-4,4);
@@ -42,18 +49,16 @@ function moveAndBounce(){
 	myBall_xPos  = myBall_xPos + myBall_xVel;
 	myBall_left  = myBall_xPos - ballSize/2;
 	myBall_right = myBall_xPos + ballSize/2;
+  // this makes the ball go back and forth horizontal
   if ((myBall_xPos > canvasWidth) || (myBall_xPos < 0)){
     myBall_xVel = -myBall_xVel;
-    switchTheColor();
   }
-
 	myBall_yPos  = myBall_yPos + myBall_yVel;
 	myBall_left  = myBall_yPos - ballSize/2;
 	myBall_right = myBall_yPos + ballSize/2;
  // this makes the ball go back and forth vertical
   if ((myBall_yPos > canvasHeight) || (myBall_yPos < 0)){
     myBall_yVel = -myBall_yVel;
-    switchTheColor();
   }
 }
 
@@ -80,12 +85,16 @@ function paddleControl(){
 }
 
 function collision(){
-if (myBall.isTouching(canvasHeight)) {
-    score= score - 1;
-  }
-   if (myBall.isTouching(paddle)) {
+  if (myBall > paddleL_yPos - paddleWidth && 
+      myBall < paddleL_yPos + paddleWidth && 
+      myBall > paddleR_yPos - paddleWidth &&
+      myBall < paddleR_yPos) {
+    myBall_xVel = -myBall_xVel;
+   }
+  if (myBall.isTouching(paddleL_yPos|| paddleR_yPos)) {
     score = score + 1;
     myBall.rotation -= 10;
+    switchTheColor();
   }
 }
 
