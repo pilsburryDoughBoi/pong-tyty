4799635577ec71fcc5d9620979a384aa0b1f3715
TyTy <Tylieafter1@gmail.com>
2020-08-03 18:14:19 +0000

    clean code

--- script.js
+++ script.js
@@ -2,28 +2,29 @@ var canvasWidth = 500, canvasHeight = 400;
 var myBall;
 var ballSize = 10;
 console.log(ballSize)
-var myBall_xPos = canvasWidth/2, 
-    myBall_yPos = canvasHeight/2, myBall_xVel = 1, myBall_yVel = 1,  
-    myBall_top = myBall_yPos - ballSize/2,
+var myBall_xPos   = canvasWidth/2, 
+    myBall_yPos   = canvasHeight/2, myBall_xVel = 1, myBall_yVel   = 1,  
+    myBall_top    = myBall_yPos - ballSize/2,
     myBall_bottom = myBall_yPos + ballSize/2, 
-    myBall_left = myBall_xPos - ballSize/2,
-    myBall_right = myBall_xPos + ballSize/2;
-var paddleWidth = 9, 
+    myBall_left   = myBall_xPos - ballSize/2,
+    myBall_right  = myBall_xPos + ballSize/2;
+var paddleWidth  = 9, 
     paddleHeight = 46, 
-    paddleVel = 5, 
-    paddleL_yPos = canvasHeight/2, 
-    paddleR_yPos = canvasHeight/2, 
-    paddleL_xPos = ballSize,
-    paddleR_xPos = canvasWidth-ballSize,
-    paddleL_top = paddleL_yPos - paddleHeight/2,
+    paddleVel    = 5, 
+    paddleL_yPos   = canvasHeight/2, 
+    paddleR_yPos   = canvasHeight/2, 
+    paddleL_xPos   = ballSize,
+    paddleL_top    = paddleL_yPos - paddleHeight/2,
 	  paddleL_bottom = paddleL_yPos + paddleHeight/2,
-	  paddleR_top = paddleR_yPos - paddleHeight/2,
+    paddleL_left   = paddleL_xPos + paddleWidth/2,
+    paddleL_right  = paddleL_xPos -  paddleWidth/2,
+    paddleR_xPos   = canvasWidth-ballSize,
+	  paddleR_top    = paddleR_yPos - paddleHeight/2,
 	  paddleR_bottom = paddleR_yPos + paddleHeight/2,
-    paddleL_left = paddleL_xPos + paddleWidth/2,
-    paddleL_right = paddleL_xPos -  paddleWidth/2,
-    paddleR_left= paddleL_xPos + paddleWidth/2,
-    paddleR_right = paddleL_xPos -  paddleWidth/2;    
-var score = 0;
+    paddleR_left   = paddleL_xPos + paddleWidth/2,
+    paddleR_right  = paddleL_xPos -  paddleWidth/2;    
+var score  = 0;
+var score2 = 0;
 var r = 0, g = 0, b = 0;
 
 
@@ -77,7 +78,8 @@ function switchTheColor(){
 }
 
 function paddleControl(){
-  paddleL_top    = paddleL_yPos - paddleHeight/2; paddleL_bottom = paddleL_yPos + paddleHeight/2; paddleR_top    = paddleR_yPos - paddleHeight/2; paddleR_bottom = paddleR_yPos + paddleHeight/2;    
+  paddleL_top    = paddleL_yPos - paddleHeight/2; paddleL_bottom = paddleL_yPos + paddleHeight/2;
+  paddleR_top    = paddleR_yPos - paddleHeight/2; paddleR_bottom = paddleR_yPos + paddleHeight/2;    
    // Update Values
   // If the value is pressed, move the paddle up on the screen
  if (keyIsDown(87) && (paddleL_top > 0)) {
@@ -93,6 +95,13 @@ function paddleControl(){
 }
 
 function collision(){ 
+  paddleL_top    = paddleL_yPos - paddleHeight/2; paddleL_bottom = paddleL_yPos + paddleHeight/2;
+  paddleR_top    = paddleR_yPos - paddleHeight/2; paddleR_bottom = paddleR_yPos + paddleHeight/2; 
+  paddleL_left   = paddleL_xPos + paddleWidth/2;
+  paddleL_right  = paddleL_xPos - paddleWidth/2;
+  paddleR_left   = paddleL_xPos + paddleWidth/2;
+  paddleR_right  = paddleL_xPos - paddleWidth/2;
+
 	if((myBall_bottom >= paddleL_top) && (myBall_top <=    paddleL_bottom)){
 		if(myBall_left <=  paddleL_right){
     		myBall_xVel = -myBall_xVel;
@@ -100,11 +109,10 @@ function collision(){
     		switchTheColor();
   		}
  	}
-
 	if((myBall_bottom >= paddleR_top) && (myBall_top <= paddleR_bottom)){
 		if(myBall_right >=  paddleR_left){
     		myBall_xVel = -myBall_xVel;
-    		score = score + 1;
+    		score2 = score2 + 1;
     		switchTheColor();
   		}
  	}
@@ -115,4 +123,8 @@ function showScore() {
   textSize(20);
   text("Score:",20,20);
   text(score,20,40);
+  fill("yellow");
+  textSize(20);
+  text("Score:",20,20);
+  text(score,40,70);
 }
